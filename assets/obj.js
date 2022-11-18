5020259bfe1403ebf7579488769ba95854610ec2
Maximo Cattaneo <cattaneomaxi@gmail.com>
2022-11-18 18:57:39 -0300

    first commit

---
+++ assets/obj.js
@@ -0,0 +1,110 @@
+const Players = [
+    {
+        id: 1,
+        nombre: "Emiliano Martinez",
+        posicion: "arquero",
+        img: "./assets/imgs/EmilianoMartinez.png"
+    },
+    {
+        id: 2,
+        nombre: "Jan Oblak",
+        posicion: "arquero",
+        img: "./assets/imgs/JanOblak.png"
+    },
+    {
+        id: 3,
+        nombre: "Thibaut Courtois",
+        posicion: "arquero",
+        img: "./assets/imgs/ThibautCourtois.png"
+    },
+    {
+        id: 4,
+        nombre: "Ederson Moraes",
+        posicion: "arquero",
+        img: "./assets/imgs/EdersonMoraes.jpg"
+    },
+    {
+        id: 5,
+        nombre: "Keylor Navas",
+        posicion: "arquero",
+        img: "./assets/imgs/KeylorNavas.jpg"
+    },
+    {
+        id: 6,
+        nombre: "Sergio Ramos",
+        posicion: "defensor",
+        img: "./assets/imgs/SergioRamos.jpg"
+    },
+    {
+        id: 7,
+        nombre: "Van Dijk",
+        posicion: "defensor",
+        img: "./assets/imgs/VanDijk.png"
+    },
+    {
+        id: 8,
+        nombre: "Cristian Romero",
+        posicion: "defensor",
+        img: "./assets/imgs/CristianRomero.jpg"
+    },
+    {
+        id: 9,
+        nombre: "Giovani Lo Celso",
+        posicion: "mediocampista",
+        img: "./assets/imgs/GiovaniLoCelso.png"
+    },
+    {
+        id: 10,
+        nombre: "Kevin De Bruyne",
+        posicion: "mediocampista",
+        img: "./assets/imgs/KevinDeBruyne.png"
+    },
+    {
+        id: 11,
+        nombre: "Carlos Casemiro",
+        posicion: "mediocampista",
+        img: "./assets/imgs/CarlosCasemiro.jpg"
+    },
+    {
+        id: 12,
+        nombre: "Paul Pogba",
+        posicion: "mediocampista",
+        img: "./assets/imgs/PaulPogba.png"
+    },
+    {
+        id: 13,
+        nombre: "Joshua Kimmich",
+        posicion: "mediocampista",
+        img: "./assets/imgs/JoshuaKimmich.png"
+    },
+    {
+        id: 14,
+        nombre: "Erling Haaland",
+        posicion: "delantero",
+        img: "./assets/imgs/ErlingHaaland.webp"
+    },
+    {
+        id: 15,
+        nombre: "Kylian Mbappé",
+        posicion: "delantero",
+        img: "./assets/imgs/KylianMbappe.png"
+    },
+    {
+        id: 16,
+        nombre: "Lionel Messi",
+        posicion: "delantero",
+        img: "./assets/imgs/LionelMessi.webp"
+    },
+    {
+        id: 17,
+        nombre: "Cristiano Ronaldo",
+        posicion: "delantero",
+        img: "./assets/imgs/CristianoRonaldo.png"
+    },
+    {
+        id: 18,
+        nombre: "Karim Benzema",
+        posicion: "delantero",
+        img: "./assets/imgs/KarimBenzema.jpg"
+    },
+]
